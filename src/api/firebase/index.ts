cf10ad81b3838865c057ef099c9f17b418e94fec
Михалев Эдуард Викторович <eduard.mikhalev@sbermegamarket.ru>
2024-12-10 19:49:03 +0300

    Развиваю мысли

--- src/api/firebase/index.ts
+++ src/api/firebase/index.ts
@@ -1,11 +1,3 @@
 import { initializeApp } from 'firebase/app'
 
-export const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyBw18CfFo6WkUT61jaVgn2z0iPpQchBZMk',
-  authDomain: 'schedule-plus-b6a78.firebaseapp.com',
-  projectId: 'schedule-plus-b6a78',
-  storageBucket: 'schedule-plus-b6a78.firebasestorage.app',
-  messagingSenderId: '8126934431',
-  appId: '1:8126934431:web:b5b6309755c5500f6be1aa',
-  measurementId: 'G-YV7E3916ZC',
-})
+export const firebaseApp = initializeApp({})
